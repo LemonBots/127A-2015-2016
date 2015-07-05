e989fab20f4155fd6081bba392dafa9dbe0e0c24
Aponthis <miller.benjamin.daniel@gmail.com>
2015-07-05 15:56:30 -0700

    Fixed errors in motors.c

--- src/motors.c
+++ src/motors.c
@@ -1,44 +1,48 @@
+# include "main.h"
+
 enum MOTORS
 {
     leftMotor1 = 1,
     leftMotor2 = 2,
     rightMotor1 = 3,
     rightMotor2 = 4
+};
+
+int leftMotor1Polarity = 1;
+int leftMotor2Polarity = -1;
+int rightMotor1Polarity = 1;
+int rightMotor2Polarity = -1;
+
+void setLeftMotor1(int power){
+	motorSet(leftMotor1, power * leftMotor1Polarity);
 }
 
-leftMotor1Polarity = 1;
-leftMotor2Polarity = -1;
-rightMotor1Polarity = 1;
-rightMotor2Polarity = -1;
-
-drive(int power){
-	setLeftMotors(power);
-	setRightMotors(power);
+void setLeftMotor2(int power){
+	motorSet(leftMotor2, power * leftMotor2Polarity);
 }
 
-setLeftMotors(int power){
+void setRightMotor1(int power){
+	motorSet(rightMotor1, power * rightMotor1Polarity);
+}
+
+void setRightMotor2(int power){
+	motorSet(rightMotor2, power * rightMotor2Polarity);
+}
+
+void setLeftMotors(int power){
 	setLeftMotor1(power);
 	setLeftMotor2(power);
 }
 
-setRightMotors(int power){
+void setRightMotors(int power){
 	setRightMotor1(power);
 	setRightMotor2(power);
 }
 
-setLeftMotor1(int power){
-	motorSet(leftMotor1, power * leftMotor1Polarity);
+void drive(int power){
+	setLeftMotors(power);
+	setRightMotors(power);
 }
 
-setLeftMotor2(int power){
-	motorSet(leftMotor2, power * leftMotor2Polarity);
-}
 
-setrightMotor1(int power){
-	motorSet(rightMotor1, power * rightMotor1Polarity);
-}
-
-setRightMotor2(int power){
-	motorSet(rightMotor2, power * rightMotor2Polarity);
-}
 
